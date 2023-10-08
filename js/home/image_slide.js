4f1415dc3d40100a8b732f6c85aead80cd0827fa
ARTH4R <137603256+ARTH4R@users.noreply.github.com>
2023-10-08 09:33:25 +0700

    updated
    อัพตอนกำลังรีบ ไม่ได้เขียนเยอะหรอก

--- js/home/image_slide.js
+++ js/home/image_slide.js
@@ -1,14 +1,28 @@
 var myIndex = 0;
-            carousel();
-            
-            function carousel() {
-              var i;
-              var x = document.getElementsByClassName("benner_under_nav_pic");
-              for (i = 0; i < x.length; i++) {
-                x[i].style.display = "none";  
-              }
-              myIndex++;
-              if (myIndex > x.length) {myIndex = 1}    
-              x[myIndex-1].style.display = "block";  
-              setTimeout(carousel, 9000    ); // Change image every 2 seconds
-            }
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("benner_under_nav_pic");
+  var dots = document.getElementsByClassName("dot");
+  
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  
+  myIndex++;
+  
+  if (myIndex > x.length) {
+    myIndex = 1;
+  }    
+  
+  x[myIndex-1].style.display = "block";  
+
+  // Update the active dot
+  for (i = 0; i < dots.length; i++) {
+    dots[i].classList.remove("active");
+  }
+  dots[myIndex - 1].classList.add("active");
+  
+  setTimeout(carousel, 9000); // Change image every 9 seconds
+}
