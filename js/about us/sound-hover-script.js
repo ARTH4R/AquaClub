8a3d735acaf4238e42acfed0aee6f6ad8f5f6a5d
ARTH4R <137603256+ARTH4R@users.noreply.github.com>
2023-11-22 11:30:12 +0700

    added asset

---
+++ js/about us/sound-hover-script.js
@@ -0,0 +1,9 @@
+function playHoverSound() {
+    const audio = document.getElementById('hover-audio');
+    audio.play();
+}
+
+function playnissan() {
+    const audio = document.getElementById('nissan');
+    audio.play();
+}
---
+++ js/home/scroll_down_button.js
@@ -0,0 +1,5 @@
+document.getElementById("scroll_down_botton") 
+  function downFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 608;
+}
