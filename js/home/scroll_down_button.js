ffbfae243ff4b4a0fd8ec9b0474a50c202ad88b6
ARTH4R <137603256+ARTH4R@users.noreply.github.com>
2023-11-22 10:54:26 +0700

    Delete js/home directory

--- js/home/scroll_down_button.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("scroll_down_botton") 
-  function downFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 608;
-}
--- js/home/sound-hover-script.js
+++
@@ -1,9 +0,0 @@
-function playHoverSound() {
-    const audio = document.getElementById('hover-audio');
-    audio.play();
-}
-
-function playnissan() {
-    const audio = document.getElementById('nissan');
-    audio.play();
-}
